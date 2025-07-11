774704bcd8dd80a2443e51b2b39c43c05a57859f
Thiago Costa <thiagomoreiracosta@gmail.com>
2025-07-10 22:54:34 -0300

    estruturacao_server_biome

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,25 @@
-import { fastify } from 'fastify'
+import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from 'fastify-type-provider-zod'
-import { fastifyCors } from '@fastify/cors'
-import { env } from './env.ts'
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: 'http://localhost:5173',
-})
+  origin: "http://localhost:5173",
+});
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
-app.get('/health', () => {
-  return 'Hello World'
-})
+app.get("/health", () => {
+  return "Hello World";
+});
 
-app.listen({ port: env.PORT}).then (() => {  
-  console.log('HTTP server running! 🚀🚀')
-})
+app.listen({ port: env.PORT }).then(() => {
+  console.log("HTTP server running! 🚀🚀");
+});
